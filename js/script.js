a1c727cb38346dd9ccc6fbba50ac190007117f1f
mattialicausi <licausivitomattia@gmail.com>
2022-10-07 17:22:53 +0200

    sistemato script per mostrare i value all'utente

--- js/script.js
+++ js/script.js
@@ -7,21 +7,21 @@
 // x va applicato uno sconto del 40% per gli over 65.
 // x L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo). Questo richiederà un minimo di ricerca.
 
-
 const btn = document.getElementById('risultato');
 
  const calcolaBiglietto = function(){
           let etaUtente = document.getElementById('eta-utente').value;
           console.log(etaUtente);
+        
 
           let kmDesiderati = document.getElementById('km-desiderati').value;
           console.log(kmDesiderati);
+          
 
           console.log(isNaN(kmDesiderati), isNaN(etaUtente));
-          // if(isNaN(kmDesiderati) || isNaN(etaUtente)){
-          //      alert('Puoi inserire solo numeri');
-          //  }
-
+          if(isNaN(kmDesiderati) || isNaN(etaUtente)){
+               alert('Puoi inserire solo numeri');
+           }
 
           const prezzoKm = 0.21;
           let prezzoFinale = 0;
@@ -33,14 +33,14 @@ const btn = document.getElementById('risultato');
           if(etaUtente < 18){
                prezzoFinale = (prezzoBiglietto * 20) / 100;
                prezzoFinale = prezzoFinale.toFixed(2);
-               // alert('Sei minorenne, hai uno sconto del 20%!');
+               document.getElementById('sconto').innerHTML = 'Sei minorenne, hai uno sconto del 20%!';
                console.log (prezzoFinale);
 
 
           }  else if(etaUtente > 65){
                prezzoFinale = (prezzoBiglietto * 40) / 100;
                prezzoFinale = prezzoFinale.toFixed(2);
-               //     alert('Hai più di 65 anni, hai uno sconto del 40%!');
+               document.getElementById('sconto').innerHTML = 'Hai più di 65 anni, hai uno sconto del 20%!';
                console.log (prezzoFinale);
 
 
@@ -50,32 +50,14 @@ const btn = document.getElementById('risultato');
                
           }
 
-          const mostraEta = function(){
-               document.getElementById('risultato-eta-utente').innerHTML = etaUtente;
-          }
-
-          const mostrakm = function(){
-               document.getElementById('risultato-km-desiderati').innerHTML = kmDesiderati;
-          }
-
-          const mostraTotale = function(){
-               document.getElementById('prezzo-finale').innerHTML = prezzoFinale;
-          }
+          // print();
+          document.getElementById('risultato-eta-utente').innerHTML = etaUtente;
+          document.getElementById('risultato-km-desiderati').innerHTML = kmDesiderati;
+          document.getElementById('prezzo-finale').innerHTML = prezzoFinale;
 }
 
-btn.addEventListener('click', calcolaBiglietto);
 
-// const elemento1 = document.getElementById('risultato-eta-utente');
-// console.log(elemento1);
-// const risultatoEta = elemento1.querySelector('div');
 
-// const elemento2 = document.getElementById('risultato-km-desiderati');
-// console.log(elemento2);
-// const risultatoKmDesiderati = elemento2.querySelector('div');
-
-// const elemento3 = document.getElementById('prezzo-finale');
-// console.log(elemento3);
-// const prezzoFinale = elemento3.querySelector('div');
 
 
 
